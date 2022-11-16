670b3509760e868f8dce2f8a615b83ba876eea4c
AganzeFelicite <aganzefelicite@gmail.com>
2022-11-16 20:25:40 +0200

    set up the test db and user with privileges

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- this is to setup the test database
+-- and the user
+
+CREATE DATABASE IF NOT EXITS hbnb_test_db;
+CREATE USER IF NOT EXITS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_pwd`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema` TO 'hbnb_test'@'localhost';
+FLASH PRIVILEGES;
