5953184634d3aa78b5f1adc59e1372bce84ec302
AganzeFelicite <aganzefelicite@gmail.com>
2022-11-16 18:01:43 +0200

    set up the mysql databse

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- creation of the database and setting up the user
+-- giving the user priviledges
+
+CREATE database IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
